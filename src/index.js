3236aee7a2511e7996d76f33a283763f5043b05e
sriegler-bioe <sriegler26@berkeley.edu>
2025-12-16 01:09:16 -0800

    PCA into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import * as Annotator from './C6-Annotator.js';
 import * as Gene from './C6-Gene.js';
 import * as Oligos from './C6-Oligos.js';
+import * as PCA from './C6-PCA.js';
 import * as Seq from './C6-Seq.js';
 import * as Sim from './C6-Sim.js';
 import * as Utils from './C6-Utils.js';
@@ -11,6 +12,7 @@ const C6 = {
   ...Annotator,
   ...Gene,
   ...Oligos,
+  ...PCA,
   ...Seq,
   ...Sim,
   ...Utils
